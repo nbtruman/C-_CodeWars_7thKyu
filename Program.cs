7b6f6d1c0befb844b6e5cf42c3d63a2b6945c244
nbtruman <104936442+nbtruman@users.noreply.github.com>
2024-06-10 12:10:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = { 1, 2, 3, 4, 5, 6};
+            int[] array2 = { 2, 3, 4 };
+            int[] result = Kata.ArrayDiff(array1, array2);
+
+            foreach(int i in result)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+public class Kata
+{
+  public static int[] ArrayDiff(int[] a, int[] b)
+  {
+    int[] subtractedArray = a.Where( i => !b.Contains(i)).ToArray();
+    
+    return subtractedArray;
+  }    
+}
+
